89f6d7aca0d95040153a6c134327976ef8519b80
Jason Hodkinson <jason.j.hodkinson@gmail.com>
2021-10-04 22:08:38 +0200

    Added in some responsive fixes for mobile when on the dashboard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
-    <div class="py-12">
+    <div class="py-12 px-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="grid grid-cols-3 gap-4">
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
                 <x-stat-card :number="$entries['students']">Students Enrolled</x-stat-card>
                 <x-stat-card :number="$entries['courses']">Courses Available</x-stat-card>
                 <x-stat-card :number="$entries['grades']">Grades Captured</x-stat-card>
