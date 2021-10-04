a24a57e6917ebd2a0b6f78cadbebf76e9668c5a8
Jason Hodkinson <jason.j.hodkinson@gmail.com>
2021-10-04 15:47:34 +0200

    Created policy rules for courses, students and grades.

---
+++ app/Policies/GradePolicy.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Grade;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class GradePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Grade  $grade
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Grade $grade)
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Grade  $grade
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Grade $grade)
+    {
+        // If the grade has a creator, only they can edit it
+        if ($grade->has('creator')) {
+            return $grade->creator->is($user);
+        }
+
+        // If no creator exists then only admins can edit it
+        return $user->is_admin;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Grade  $grade
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Grade $grade)
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Grade  $grade
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Grade $grade)
+    {
+        return $user->is_admin;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Grade  $grade
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Grade $grade)
+    {
+        return $user->is_admin;
+    }
+}
