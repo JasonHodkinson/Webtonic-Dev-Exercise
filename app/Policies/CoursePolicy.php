c34f4b420b87a711b2e917f3e1b486902d82ec8e
Jason Hodkinson <jason.j.hodkinson@gmail.com>
2021-10-05 22:00:25 +0200

    Added in missing policy methods for the CoursePolicy.

--- app/Policies/CoursePolicy.php
+++ app/Policies/CoursePolicy.php
@@ -11,6 +11,31 @@ class CoursePolicy
     use HandlesAuthorization;
 
     /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user)
+    {
+        // Anyone can view the courses
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Course  $course
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Course $course)
+    {
+        // Anyone can view a course
+        return true;
+    }
+
+    /**
      * Determine whether the user can create models.
      *
      * @param  \App\Models\User  $user
