0aa13fae55327dccc1c3c8245d4fcd1d43f8e2e6
Jason Hodkinson <jason.j.hodkinson@gmail.com>
2021-10-05 15:14:33 +0200

    Updated the look and feel of the file input in the grades uploader page.

--- resources/views/grades/upload.blade.php
+++ resources/views/grades/upload.blade.php
@@ -2,18 +2,33 @@
     <div class="py-12 px-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <form action="{{ route('grades.import') }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <div class="col-span-6">
-                            <label for="grades" class="block text-sm font-medium text-gray-700">Upload Grades</label>
-                            <input type="file" accept=".csv" name="grades" class="mt-1">
+                <form action="{{ route('grades.import') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <h1>Grades Uploader</h1>
+                        <hr class="my-4">
+                        <div class="mb-4">
+                            <label class="mb-1 block text-sm font-medium text-gray-800" for="csv_document">
+                                Upload a .csv file
+                            </label>
+                            <div class="relative">
+                                <x-file-input required name="csv_document" accept=".csv" />
+                            </div>
+                            @error('csv_document')
+                            <div class="text-red-600 text-sm mt-1">{{ $message }}</div>
+                            @enderror
+                            <div class="mt-1 text-sm text-gray-500">
+                                Please note that the delimeter of the .csv should be the <code>;</code> symbol.
+                            </div>
                         </div>
-                        <div class="text-right">
-                            <x-button type="submit" class="text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Submit</x-button>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                    <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                        <button type="submit"
+                            class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            Import
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
