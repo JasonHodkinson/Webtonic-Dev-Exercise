241f041b1d9d215ef815b7263baa1a5e4d82ab72
Jason Hodkinson <jason.j.hodkinson@gmail.com>
2021-10-05 22:01:07 +0200

    Added in a show page for the courses which also displays all related grades.

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -85,6 +85,8 @@ class CourseController extends Controller
      */
     public function show(Course $course)
     {
+        $course->load('grades.student');
+        
         return view('courses.show', compact('course'));
     }
 
